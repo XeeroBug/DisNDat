680d15038751ddb62677075872fb8cc77fc6fee4
bin-abdoul <usamaabdulhakeem2006@gmail.com>
2025-09-01 16:18:34 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Logo from "@/assets/logo.png";
+function App() {
+  return (
+    <div className="">
+      <div className="py-10 px-25">
+        <h1 className="flex gap-2 font-bold text-[32px]">
+          <img src={Logo} alt="" />
+          DisNDat
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
